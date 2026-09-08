3752fb1c1ddc44bc9be79f112b9d123fceeea433
catalina <catalina.duran2025@alu.uct.cl>
2026-09-08 16:46:47 -0300

    feat(db): crea tabla organizations S2-014

--- database/migrations/create_organizations_table.sql
+++ database/migrations/create_organizations_table.sql
@@ -1,12 +1,13 @@
-CREATE TABLE organizations (
+CREATE TABLE public.organizations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
     description TEXT,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT TIMEZONE('utc'::text, NOW()) NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT TIMEZONE('utc'::text, NOW()) NOT NULL
 );
 
+-- Reutilizamos la función de Mathias para actualizar la fecha
 CREATE TRIGGER set_organizations_updated_at
-BEFORE UPDATE ON organizations
+BEFORE UPDATE ON public.organizations
 FOR EACH ROW
-EXECUTE FUNCTION nombre_de_la_funcion_de_mathias();
+EXECUTE FUNCTION public.handle_updated_at();
